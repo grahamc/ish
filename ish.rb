8fb247c8a524eba582c46b2457b31743d355dcb4
Graham Christensen <graham@grahamc.com>
2015-09-24 09:37:51 -0500

    Updated dependencies for brew

--- ish.rb
+++ ish.rb
@@ -1,6 +1,6 @@
 class Ish < Formula
   url "https://github.com/grahamc/ish.git", :ref => 'master'
-  version '0.1.0'
+  version '0.1.1'
 
   depends_on :python3
 
@@ -9,12 +9,42 @@ class Ish < Formula
     sha1 "be8bb2154af0589d4a19231412cfec07c60252b3"
   end
 
+  resource "botocore" do
+    url "https://pypi.python.org/packages/source/b/botocore/botocore-1.2.4.tar.gz"
+    sha1 "22d04c5adaa01ac3e5229b5ece14785799b1add9"
+  end
+
+  resource "docutils" do
+    url "https://pypi.python.org/packages/source/d/docutils/docutils-0.12.tar.gz"
+    sha1 "002450621b33c5690060345b0aac25bc2426d675"
+  end
+
+  resource "futures" do
+    url "https://pypi.python.org/packages/source/f/futures/futures-2.2.0.tar.gz"
+    sha1 "0302253fb7e4fbbc48000b3e3dde244e9e7cd353"
+  end
+
+  resource "jmespath" do
+    url "https://pypi.python.org/packages/source/j/jmespath/jmespath-0.7.1.tar.gz"
+    sha1 "10cbbf7f1a892d2c4fce7f7e84bb91e9108fcf45"
+  end
+
+  resource "python-dateutil" do
+    url "https://pypi.python.org/packages/source/p/python-dateutil/python-dateutil-2.4.2.tar.gz"
+    sha1 "1d975f5db65306a61f4353ef00308ec806f47f54"
+  end
+
+  resource "six" do
+    url "https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz"
+    sha1 "d168e6d01f0900875c6ecebc97da72d0fda31129"
+  end
+
   def install
     version = Language::Python.major_minor_version "python3"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{version}/site-packages"
 
 
-    %w[boto3].each do |r|
+    %w[boto3 botocore docutils futures jmespath python-dateutil six].each do |r|
       resource(r).stage do
         system "python3", *Language::Python.setup_install_args(libexec/"vendor")
       end
